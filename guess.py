5bfb09e3b7908d2598a6dbaae6ca622c9a9fa657
TTheHolyOne <69742571+TTheHolyOne@users.noreply.github.com>
2021-04-17 21:31:21 -0500

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,26 +1,35 @@
 import random
-import os
-
-os.system('cls')
-print('Welcome to number guesser!\n')
+import time
+print('Welcome to number guesser!\nHave fun!')
+numgen = random.randrange(1,100)
+guesses = 0
 while True:
-	rannum = random.randrange(0, 101)
-	i = input(f'Please choose number through 1-100 or enter q to quit\n')
+	time.sleep(1)
+	i = input('Please choose number through 1-100 otherwise enter q to quit or enter p to reset\n')
+	while not (i.lower() in ['p', 'q'] or i.isdigit() and 0 < int(i) < 100):
+		i = input('Please enter Q to quit or 1-100 \n')
 	if i.lower() == 'q':
 		break
 	if i.lower() == 'p':
+		numgen = random.randrange(1,100)
+		guesses = 0
 		continue
-	if int(i) == rannum:
-		print(f'You guessed correctly!\nThe number was {rannum}\nYou answered {i}\nEnter p to play again!\nEnter q to quit!')
-		input()
-	elif int(i) < rannum:
-		print(f'Sorry but you too low :(\nYour guess was {i}')
-		continue
-		input()
-	else: 
-		print('Your guess is too high')
-		continue
-		input()
-	input()
 
-print('Shutting Down... ')
+	if int(i) == numgen:
+		time.sleep(1)
+		guesses += 1
+		print(f'You guessed correctly in {guesses} guesses!\nYour number was {i} and the answer is is {numgen}\n')
+		guesses = 0
+		numgen = random.randrange(1,100)
+	elif int(i) < numgen:
+		time.sleep(1)
+		print(f'You guessed to low\nYour number you guessed was {i}')
+		guesses += 1
+	elif int(i) > numgen:
+		time.sleep(1)
+		print(f'You guessed to high\nYour number you guessed was {i}')
+		guesses += 1
+
+	
+
+print('Shutting Down..')
